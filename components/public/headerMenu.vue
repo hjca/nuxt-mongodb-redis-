46ff3020674fed46a74594a1cdb11e0fd2fec7fa
吴辉 <13716055457@163.com>
2019-06-10 18:58:30 +0800

    添加图标

--- components/public/headerMenu.vue
+++ components/public/headerMenu.vue
@@ -4,22 +4,31 @@
     <el-dropdown>
       <span class="el-dropdown-link">
         我的酒仙
-        <i class="el-icon-arrow-down el-icon--right"/>
+        <i class="el-icon-caret-bottom el-icon--right"/>
       </span>
-      <el-dropdown-menu slot="dropdown">
-        <el-dropdown-item>
+      <el-dropdown-menu slot="dropdown" class="default-menu-style">
+        <el-dropdown-item class="default-menu-style-item">
           <nuxt-link to="/">我的订单</nuxt-link>
         </el-dropdown-item>
 
-        <el-dropdown-item>
+        <el-dropdown-item class="default-menu-style-item">
           <nuxt-link to="/">物流跟踪</nuxt-link>
         </el-dropdown-item>
 
-        <el-dropdown-item>
+        <el-dropdown-item class="default-menu-style-item">
           <nuxt-link to="/">我的优惠券</nuxt-link>
         </el-dropdown-item>
       </el-dropdown-menu>
     </el-dropdown>
+
+    <!-- CEO邮箱 -->
+    <nuxt-link to="/" class="menu-href">CEO邮箱</nuxt-link>
+
+    <!-- 购物车 -->
+    <nuxt-link to="/">
+      购物车0件
+      <i class=""/>>
+    </nuxt-link>
   </div>
 </template>
 
@@ -48,9 +57,16 @@ export default {
   }
   .el-dropdown{
     cursor: pointer;
+    margin-right: 12px;
     span:hover{
       color: #c00;
     }
+  }
+  .menu-href{
+    font-size: 12px;
+    color: #999999;
+    margin-right: 12px;
+    color: #c00;
   }
 }
 </style>
